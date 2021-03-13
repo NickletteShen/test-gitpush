d84997aff0c1fb1e667f651aa3b94eb666932eb5
NickletteShen <287783769@qq.com>
2021-03-13 17:45:27 +0800

    别人的代码

---
+++ gulpfile.js
@@ -0,0 +1,118 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const jade = require('gulp-jade')
+const copy = require('gulp-copy')
+const rimrafPromise = require('rimraf-promise')
+const ghPages = require('gulp-gh-pages')
+const fs = require('fs')
+const connect = require('gulp-connect')
+const generatePdf = require('./generate_pdf')
+
+gulp.task('resume-sass', () => {
+  gulp
+    .src('src/scss/resume.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ['last 4 versions'],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest('dist/css/'))
+    .pipe(connect.reload())
+})
+
+gulp.task('icon-sass', () => {
+  gulp
+    .src('src/scss/iconfont.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ['last 4 versions'],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest('dist/iconfont/'))
+    .pipe(connect.reload())
+})
+
+gulp.task('sass:watch', () => {
+  gulp.watch('./src/scss/resume.scss', ['resume-sass'])
+  gulp.watch('./src/scss/iconfont.scss', ['icon-sass'])
+  gulp.watch('./src/scss/components/*.scss', ['resume-sass'])
+})
+
+gulp.task('json2jade', () => {
+  const info = JSON.parse(fs.readFileSync('./info.json', 'utf-8'))
+  const locals = highlight(info)
+  gulp
+    .src('./src/jade/index.jade')
+    .pipe(
+      jade({
+        locals
+      })
+    )
+    .pipe(gulp.dest('./dist/'))
+    .pipe(connect.reload())
+})
+
+gulp.task('json2jade:watch', () => {
+  gulp.watch('./info.json', ['json2jade'])
+})
+
+function src2dist(dir) {
+  return gulp.src(`./src/${dir}/*.*`).pipe(gulp.dest(`./dist/${dir}/`))
+}
+
+function highlight(locals) {
+  var locals = JSON.stringify(locals)
+  const re = /`(.+?)`/g
+  locals = locals.replace(re, '<strong>$1</strong>')
+  return JSON.parse(locals)
+}
+
+gulp.task('copy', () => {
+  src2dist('iconfont')
+  src2dist('img')
+  src2dist('pdf')
+  gulp.src('./CNAME').pipe(gulp.dest('./dist'))
+})
+
+gulp.task('clean', () => {
+  rimrafPromise('./dist/')
+})
+
+gulp.task('deploy', () =>
+  gulp.src('./dist/**/*').pipe(
+    ghPages({
+      remoteUrl: 'git@git.dev.tencent.com:tybleo/LeoTianResumeDist.git',
+      branch: 'master'
+    })
+  )
+)
+
+let port = 9000
+
+// 避免打印时，同时运行开发服务报错
+gulp.task('set-pdf-port', () => {
+  port = 9001
+})
+
+gulp.task('webserver', () => {
+  connect.server({
+    root: './dist',
+    livereload: true,
+    port
+  })
+})
+
+gulp.task('dev', ['default', 'json2jade:watch', 'sass:watch', 'webserver'])
+
+gulp.task('default', ['icon-sass', 'resume-sass', 'json2jade', 'copy'])
+
+gulp.task('pdf', ['set-pdf-port', 'default', 'webserver'], async () => {
+  await generatePdf('http://localhost:9001')
+  connect.serverClose()
+  process.exit(0)
+})
---
+++ readme.md
@@ -0,0 +1,39 @@
+# Introduction
+
+简历为仿写，原简历页面地址: [张秋怡 - 个人简历](https://joyeecheung.github.io/resume/) 【原主人已删除】
+
+简历基于 Gulp+Scss+Jade。
+访问地址：[LeoTian 的简历](https://resume.leotian.cn)
+
+### 要求
+
+**Node 8.9.0 及以上**
+
+## 启动
+
+使用 `npm install` 安装
+
+**npm + 国内源** 可能会出现 chrome 安装的问题，推荐使用 `cnpm` 或者 `npm` + 原版源的方式
+
+### 特点
+
+自动生成 PDF 版简历
+
+在 generate_pdf 与 info.json 中，替换 LxxyxResume.pdf 为你自己想要的名字
+
+生成 PDF 的命令：
+
+```bash
+npm run pdf
+```
+
+### 运行方式：
+
+替换 info.json 的内容，运行`gulp`即可。
+生成内容可在 dist 文件夹查看。
+
+需要自动部署的，运行`gulp deploy`即可，需要在 gulpfile 中更改为你的地址（！会抹去你 github pages 内容，慎用！）
+
+### 自定义域名
+
+修改 CNAME 文件，替换为你的域名即可
